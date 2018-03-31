c84e70e19fabd4ca934ffa2979e32a62fe5a5efe
LarssonS <Stefanlarsson95@hotmail.com>
2018-04-01 00:32:17 +0200

    .

--- scripts/main.js
+++ scripts/main.js
@@ -9,3 +9,17 @@ toggleNav.addEventListener("click", function(){
 		navBar.classList.remove("nav-bar-open");
 	}
 })
+
+//AIzaSyDm-vQBC_7g_HRGBkLOOk4fkJqSa6UJgpc api key
+
+function initMap() {
+	var office = { lat: 59.33066993230202, lng: 18.06765197952768 };
+	var map = new google.maps.Map(document.getElementById('map'), {
+		zoom: 13,
+		center: office
+	});
+	var marker = new google.maps.Marker({
+		position: office,
+		map: map
+	});
+}
